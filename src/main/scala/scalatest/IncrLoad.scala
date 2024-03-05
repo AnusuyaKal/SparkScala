2fb3607f242173971650594f0a66e50042b14b1d
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-05 20:10:23 +0000

    Update IncrLoad.scala

--- src/main/scala/scalatest/IncrLoad.scala
+++ src/main/scala/scalatest/IncrLoad.scala
@@ -18,7 +18,7 @@ object IncrLoad {
 
     try {
       // Read existing data from Hive table
-      val existingData = spark.sql("SELECT * FROM health_insurance")
+      val existingData = spark.sql("SELECT * FROM USUK30.people")
 
       // Read new data from PostgreSQL
       val newData = spark.read.jdbc(postgresUrl, "health_insurance", postgresProperties)
@@ -30,7 +30,7 @@ object IncrLoad {
         println("No new data to load. Incremental load test passed.")
       } else {
         // Append new data to Hive table
-        incrementalData.write.mode("append").saveAsTable("health_insurance")
+        incrementalData.write.mode("append").saveAsTable("USUK30.people")
         println("Incremental load successful.")
       }
     } catch {
