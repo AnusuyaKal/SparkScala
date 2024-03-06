08d9fe0f976aab6a059880c9a9a761757515ff3a
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-06 22:37:04 +0000

    Update IncrLoad.scala

--- src/main/scala/scalatest/IncrLoad.scala
+++ src/main/scala/scalatest/IncrLoad.scala
@@ -23,7 +23,8 @@ object IncrLoad {
       // Read new data from PostgreSQL
       // val newData = spark.read.jdbc(postgresUrl, "car_insurance_claims", postgresProperties)
       
-      val whereCondition = "'AGE' > 73"
+      // val whereCondition = "'AGE' > 73"
+      val whereCondition = """"AGE" > 73"""
       // Read new data from PostgreSQL with the WHERE condition
       // val newData = spark.read.jdbc(postgresUrl, "car_insurance_claims", postgresProperties, predicates = Array(whereCondition))
       // val newData = spark.read.jdbc(postgresUrl, "car_insurance_claims", postgresProperties, predicates = Map("predicates" -> whereCondition))
