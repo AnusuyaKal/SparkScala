8a2ce93860a2d77cf12f97c284abfc4c37c28bdd
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-06 17:02:04 +0000

    Update IncrLoad.scala

--- src/main/scala/scalatest/IncrLoad.scala
+++ src/main/scala/scalatest/IncrLoad.scala
@@ -36,7 +36,7 @@ object IncrLoad {
         println("No new data to load. Incremental load test passed.")
       } else {
         // Append new data to Hive table
-        incrementalData.write.mode("append").saveAsTable("USUK30.people")
+        incrementalData.write.mode("overwrite").saveAsTable("USUK30.people")
         println("Incremental load successful.")
       }
     } catch {
