b01f37c44126f19e08a2bb24d8cdaab9e39a1d7e
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-07 16:26:41 +0000

    Update Kafka_Test.scala

--- src/main/scala/scalatest/Kafka_Test.scala
+++ src/main/scala/scalatest/Kafka_Test.scala
@@ -1,28 +1,28 @@
 import org.apache.spark.sql.SparkSession
 
-object Kafka_Test {
+object Kafka_Test_Test {
 
   def main(args: Array[String]): Unit = {
     // Create a SparkSession for testing
     val spark = SparkSession.builder()
-      .appName("MyMainClassTest")
+      .appName("Kafka_Test_Test")
       .master("local[2]")
       .getOrCreate()
 
-    // Test case for MyMainClass functionality
-    testMyMainClassFunctionality(spark)
+    // Test case for Kafka_Test functionality
+    testKafka_TestFunctionality(spark)
 
     // Stop SparkSession after test
     spark.stop()
   }
 
-  // Test MyMainClass functionality
-  def testMyMainClassFunctionality(spark: SparkSession): Unit = {
+  // Test Kafka_Test functionality
+  def testKafka_TestFunctionality(spark: SparkSession): Unit = {
     val url = "http://18.133.73.36:5001/insurance_claims1"
     val topic = "insurance_claims_5-3-12-98"
 
-    // Execute MyMainClass code
-    MyMainClass.main(Array())
+    // Execute Kafka_Test code
+    Kafka_Test.main(Array())
 
     // Read the DataFrame from Kafka topic
     val kafkaDF = spark.read
