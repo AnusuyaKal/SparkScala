9d23b725260444aefe888980932c943aa92a538f
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-11 12:23:47 +0000

    Update Kafka_Test.scala

--- src/main/scala/scalatest/Kafka_Test.scala
+++ src/main/scala/scalatest/Kafka_Test.scala
@@ -1,5 +1,13 @@
 import scala.sys.process._
+package scalatest;
 
+import scala.io.Source
+import org.apache.spark.sql.SparkSession
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Put, Admin}
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName, HColumnDescriptor, HTableDescriptor}
+
+object Kafka_Test extends App{
 // Define the HBase table name
 val tableName = "insurance_claims_5-3-12-98"
 
@@ -14,3 +22,4 @@ val rowCount = output.trim.split(" ").head.toLong
 
 // Print the row count
 println(s"Number of rows in HBase table '$tableName': $rowCount")
+}
