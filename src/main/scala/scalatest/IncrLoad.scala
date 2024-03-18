1b32bc93f3e3ece29b8597d298cfada4ebff6d82
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-18 21:22:35 +0000

    Update IncrLoad.scala

--- src/main/scala/scalatest/IncrLoad.scala
+++ src/main/scala/scalatest/IncrLoad.scala
@@ -1,73 +1,73 @@
-package scalatest
+// package scalatest
 
-import org.apache.spark.sql.SparkSession
+// import org.apache.spark.sql.SparkSession
 
-object IncrLoad {
-  def main(args: Array[String]): Unit = {
-    // Create SparkSession
-    val spark = SparkSession.builder()
-      .appName("IncrementalLoadTest")
-      .getOrCreate()
+// object IncrLoad {
+//   def main(args: Array[String]): Unit = {
+//     // Create SparkSession
+//     val spark = SparkSession.builder()
+//       .appName("IncrementalLoadTest")
+//       .getOrCreate()
 
-    // Define PostgreSQL connection properties
-    val postgresUrl = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
-    val postgresProperties = new java.util.Properties()
-    postgresProperties.put("user", "consultants")
-    postgresProperties.put("password", "WelcomeItc@2022")
-    postgresProperties.put("driver", "org.postgresql.Driver")
+//     // Define PostgreSQL connection properties
+//     val postgresUrl = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
+//     val postgresProperties = new java.util.Properties()
+//     postgresProperties.put("user", "consultants")
+//     postgresProperties.put("password", "WelcomeItc@2022")
+//     postgresProperties.put("driver", "org.postgresql.Driver")
 
-    try {
-      // Read existing data from Hive table
-      val existingData = spark.read.format("parquet").table("usukprjdb.people") // Read the existing table directly
+//     try {
+//       // Read existing data from Hive table
+//       val existingData = spark.read.format("parquet").table("usukprjdb.people") // Read the existing table directly
 
-      // Read new data from PostgreSQL
-      // val newData = spark.read.jdbc(postgresUrl, "people2", postgresProperties)
+//       // Read new data from PostgreSQL
+//       // val newData = spark.read.jdbc(postgresUrl, "people2", postgresProperties)
       
-      val whereCondition = """"people_id" > 11"""
-      // Read new data from PostgreSQL with the WHERE condition
-      // val newData = spark.read.jdbc(postgresUrl, "car_insurance_claims", postgresProperties, predicates = Array(whereCondition))
-      // val newData = spark.read.jdbc(postgresUrl, "car_insurance_claims", postgresProperties, predicates = Map("predicates" -> whereCondition))
-      // val newData = spark.read.jdbc(s"$postgresUrl?user=consultants&password=WelcomeItc@2022&$whereCondition", "car_insurance_claims", postgresProperties)
-      val query = s"(SELECT * FROM people2 WHERE $whereCondition) AS data"
-      val newData = spark.read.jdbc(postgresUrl, query, postgresProperties)
+//       val whereCondition = """"people_id" > 11"""
+//       // Read new data from PostgreSQL with the WHERE condition
+//       // val newData = spark.read.jdbc(postgresUrl, "car_insurance_claims", postgresProperties, predicates = Array(whereCondition))
+//       // val newData = spark.read.jdbc(postgresUrl, "car_insurance_claims", postgresProperties, predicates = Map("predicates" -> whereCondition))
+//       // val newData = spark.read.jdbc(s"$postgresUrl?user=consultants&password=WelcomeItc@2022&$whereCondition", "car_insurance_claims", postgresProperties)
+//       val query = s"(SELECT * FROM people2 WHERE $whereCondition) AS data"
+//       val newData = spark.read.jdbc(postgresUrl, query, postgresProperties)
 
-      newData.show()
+//       newData.show()
 
 
 
-      // Identify new rows by performing a left anti join
-      // val incrementalData = newData.join(existingData, newData.columns, "left_anti")
-      val incrementalData = newData
-      // incrementalData.show()
-      println("New_Count", newData.count()) 
-      println("Existing_Count", existingData.count())
-      println("Incremented_Count", incrementalData.count())
+//       // Identify new rows by performing a left anti join
+//       // val incrementalData = newData.join(existingData, newData.columns, "left_anti")
+//       val incrementalData = newData
+//       // incrementalData.show()
+//       println("New_Count", newData.count()) 
+//       println("Existing_Count", existingData.count())
+//       println("Incremented_Count", incrementalData.count())
       
-      // if (incrementalData.count() == newData.count() + existingData.count()){
-      //   println("Count Matches")
-      // else:
-      //   println("Not Matches")
-      // }
-      if (incrementalData.count() == newData.count() + existingData.count()) {
-        println("Count Matches")
-      } else {
-        println("Not Matches")
-      }
+//       // if (incrementalData.count() == newData.count() + existingData.count()){
+//       //   println("Count Matches")
+//       // else:
+//       //   println("Not Matches")
+//       // }
+//       if (incrementalData.count() == newData.count() + existingData.count()) {
+//         println("Count Matches")
+//       } else {
+//         println("Not Matches")
+//       }
 
 
-      if (incrementalData.isEmpty) {
-        println("No new data to load. Incremental load test passed.")
-      } else {
-        // Append new data to Hive table
-        incrementalData.write.mode("append").format("parquet").saveAsTable("usukprjdb.people")
-        println("Incremental load successful.")
-      }
-    } catch {
-      case e: Exception =>
-        println(s"Test failed: ${e.getMessage}")
-    } finally {
-      // Stop SparkSession after testing
-      spark.stop()
-    }
-  }
-}
+//       if (incrementalData.isEmpty) {
+//         println("No new data to load. Incremental load test passed.")
+//       } else {
+//         // Append new data to Hive table
+//         incrementalData.write.mode("append").format("parquet").saveAsTable("usukprjdb.people")
+//         println("Incremental load successful.")
+//       }
+//     } catch {
+//       case e: Exception =>
+//         println(s"Test failed: ${e.getMessage}")
+//     } finally {
+//       // Stop SparkSession after testing
+//       spark.stop()
+//     }
+//   }
+// }
