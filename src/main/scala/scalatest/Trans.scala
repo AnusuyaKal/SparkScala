a560bc1870dc43047fe04cb7416737ac52988a4c
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-18 22:09:07 +0000

    Update Trans.scala

--- src/main/scala/scalatest/Trans.scala
+++ src/main/scala/scalatest/Trans.scala
@@ -3,7 +3,7 @@ package scalatest
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-object FullLoadTest {
+object Trans {
   def main(args: Array[String]): Unit = {
     // Create SparkSession for testing
     val spark: SparkSession = SparkSession.builder()
