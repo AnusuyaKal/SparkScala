51f2c50c874263a1a2a661f8746fca1f06951db2
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-19 21:41:13 +0000

    Update Kafka_Test.scala

--- src/main/scala/scalatest/Kafka_Test.scala
+++ src/main/scala/scalatest/Kafka_Test.scala
@@ -121,7 +121,39 @@ object Kafka_Test extends App {
   println(s"HBase table is  : $tableName")
   println("----------------------------")
 
-  // Close HBase table and connection to clean up resources
-  table.close()
-  connection.close()
+// HBase configuration
+     val hbaseConf = HBaseConfiguration.create()
+     hbaseConf.set("hbase.zookeeper.quorum", "ip-172-31-3-80.eu-west-2.compute.internal,ip-172-31-5-217.eu-west-2.compute.internal,ip-172-31-9-237.eu-west-2.compute.internal")
+     hbaseConf.set("hbase.zookeeper.property.clientPort", "2181")
+     val connection = ConnectionFactory.createConnection(hbaseConf)
+     val tableName = TableName.valueOf("Insurance_Claim_11_3_11h_50")
+     val table = connection.getTable(tableName)
+// Scan the table to count the rows
+     val scan = new Scan()
+     val scanner: ResultScanner = table.getScanner(scan)
+     var rowCount: Int = 0
+
+     try {
+       println("Counting rows...")
+       val iterator = scanner.iterator()
+       while (iterator.hasNext) {
+         val result = iterator.next()
+         if (!result.isEmpty) {
+           rowCount += 1
+         }
+       }
+    } finally {
+      scanner.close()
+    }
+     print("################## RESULT #####################")
+     println(s"Total number of rows in the table: $rowCount")
+     print("################## RESULT #####################")
+   }
+
+
+ }
+
+// Close HBase table and connection to clean up resources
+table.close()
+connection.close()
 }
