862adff944d97bffa1d60c3dfbce17fe4abc4e1e
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-03-20 11:11:48 +0000

    Update IncrLoad_Count_Check.scala

--- src/main/scala/scalatest/IncrLoad_Count_Check.scala
+++ src/main/scala/scalatest/IncrLoad_Count_Check.scala
@@ -1,50 +1,48 @@
-package scalatest;
+package scalatest
+
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-object IncrLoad_Count_Check {
+object TestIncrDataLoading {
   def main(args: Array[String]): Unit = {
-    // Create SparkSession
-    val spark = SparkSession.builder()
-      .appName("IncrementalLoadTest")
+    // Initialize SparkSession
+    val spark = SparkSession.builder
+      .appName("TestDataLoading")
+      .master("local[2]")
+      .enableHiveSupport()
       .getOrCreate()
 
-    // Define PostgreSQL connection properties
-    val postgresUrl = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
-    val postgresProperties = new java.util.Properties()
-    postgresProperties.put("user", "consultants")
-    postgresProperties.put("password", "WelcomeItc@2022")
-    postgresProperties.put("driver", "org.postgresql.Driver")
-
     try {
-      // Read new data schema from PostgreSQL to get column names
-      val newDataSchema = spark.read.jdbc(postgresUrl, "people", postgresProperties).schema
+      // Read data from PostgreSQL
+      val postgresUrl = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
+      val postgresProperties = new java.util.Properties()
+      postgresProperties.put("user", "consultants")
+      postgresProperties.put("password", "WelcomeItc@2022")
+      postgresProperties.put("driver", "org.postgresql.Driver")
+      
+      val whereCondition = """"people_id" > 14"""
+      val query = s"(SELECT * FROM public.people WHERE $whereCondition) AS data"
+      println("Generated SQL query:", query)
 
-      // Define the column name transformation logic
-      def transformColumn(name: String): String = {
-        // Add your transformation logic here, for example:
-        // Convert column names to upper case
-        name.toUpperCase
-      }
-
-      // Apply column name transformation to the new data DataFrame
-      val transformedData = newDataSchema.fields.foldLeft(spark.read.jdbc(postgresUrl, "people", postgresProperties)) {
-        (df: DataFrame, field) => df.withColumnRenamed(field.name, transformColumn(field.name))
-      }
-
-      val whereCondition = """"people_id" > 30"""  // Define your WHERE condition here
+      // Read new data from PostgreSQL with the WHERE condition
+      val postData = spark.read.jdbc(postgresUrl, query, postgresProperties)
+      postData.show()
+      val postgresCount = postData.count()
 
       // Read count of rows from Hive table after applying the WHERE condition
-      val hiveQuery = s"SELECT COUNT(*) AS count FROM usukprjdb.people WHERE $whereCondition"
-      val hiveCountDF = spark.sql(hiveQuery)
+      val hiveWhereCondition = "people_id > 14"
+      val hiveCountDF = spark.sql(s"SELECT COUNT(*) AS count FROM usukprjdb.people WHERE $hiveWhereCondition")
       val hiveCount = hiveCountDF.collect()(0)(0).toString.toLong
 
-      println("Hive Count after incremental load:", hiveCount)
+      // Print updated count
+      println("Updated_count:", hiveCount)
 
-      // Read count of rows from PostgreSQL table after applying the same WHERE condition
-      val postgresCountDF = transformedData.where(whereCondition)
-      val postgresCount = postgresCountDF.count()
-
-      println("PostgreSQL Count after incremental load:", postgresCount)
+      if (hiveCount == postgresCount) {
+        println("Number of rows loaded to Hive matches the expected count")
+        println("Postgres_Count: " + postgresCount)
+        println("Hive_count: " + hiveCount)
+      } else {
+        assert(hiveCount == postgresCount, "Number of rows loaded to Hive does not match expected count")
+      }
     } catch {
       case e: Exception =>
         println(s"Test failed: ${e.getMessage}")
